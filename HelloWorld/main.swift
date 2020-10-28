306730cb080e8491d49f66aedcb0a35aa953fce0
s3747380 <s3747380@rmit.edu.vn>
2020-10-28 14:21:20 +0700

    hello

---
+++ HelloWorld/main.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  HelloWorld
+//
+//  Created by Danh, Le Ngoc on 10/28/20.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("Fuck you" , "David")
+
+var myAge: Int = 21
+let myConstant: Int = 22
+
+print("How old am I? ", myAge)
+
+
+
+var sentence = "This is a \nsentencewith a \""
+let dollarSign = "\u{24}"
+let sparklingHeart: Character = "\u{1F496}"
+
+print (sentence)
+
+print(dollarSign)
+
+print(sparklingHeart)
+
+
+let greeting = "Hello, World!"
+print("Number of characters is", greeting.count)
+for index in greeting.indices{
+    print(index, greeting[index])
+}
+// this is my comment
